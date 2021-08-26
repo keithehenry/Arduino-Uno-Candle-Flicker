ee179a8100eed482bcde473d181615a1cd0826e7
Keith E Henry <keithehenry@gmail.com>
2021-08-26 11:13:51 -0700

    Tested and working.

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+// https://youtu.be/OxSUIeZs2d8
+// https://youtu.be/uX9YMCLgK9A
+// Project 9 - LED Fire Effect
+int ledPin1 = 9;
+int ledPin2 = 10;
+int ledPin3 = 11;
+
+void setup()
+{
+ pinMode(ledPin1, OUTPUT);
+ pinMode(ledPin2, OUTPUT);
+ pinMode(ledPin3, OUTPUT);
+}
+
+void loop()
+{
+ analogWrite(ledPin1, random(120)+135);
+ analogWrite(ledPin2, random(120)+135);
+ analogWrite(ledPin3, random(120)+135);
+ delay(random(100));
+}
